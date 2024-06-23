bcbb5fe19dcc9a6e292e13c7101d482e501f4d90
vrushabh <vrushabh>
2024-06-23 16:28:03 +0530

    ss

--- vpc.tf
+++ vpc.tf
@@ -45,10 +45,10 @@ resource "aws_route" "default_route" {
 }
 
 
-resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
-}
+# resource "aws_route_table_association" "main" {
+#   subnet_id      = aws_subnet.main.id
+#   route_table_id = aws_route_table.main.id
+# }
 
 
 
